381286df5857003bdaf9c1fdf90c57bf05d59250
Cigno05 <paolocalcagni.82@gmail.com>
2024-05-06 17:02:36 +0200

    ADD Bootstrap and table

--- index.php
+++ index.php
@@ -40,66 +40,75 @@ $hotels = [
 
 ];
 
-for ($i = 0; $i < count($hotels); $i++) {
-    $hotel = $hotels[$i];
-    // var_dump($hotel);
-    $name = $hotel['name'];
-    // var_dump($name);
-    $description = $hotel['description'];
+?>
 
-    $parking = $hotel['parking'];
-    if ($parking === true) {
-        $parkingCheck = "È presente un parcheggio";
-    } else {
-        $parkingCheck = "Non è presente un parcheggio";
-    };
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>PHP-Hotel</title>
+</head>
 
-    $vote = $hotels['vote'];
-    $distance = $hotel['distance_to_center'];
+<body>
 
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Voto</th>
+                <th scope="col">Distanza dal centro</th>
+            </tr>
+        </thead>
+        <tbody>
 
+<?php
 
+            for ($i = 0; $i < count($hotels); $i++) {
+                $hotel = $hotels[$i];
+                // var_dump($hotel);
+                $name = $hotel['name'];
+                $description = $hotel['description'];
+                
+                $parking = $hotel['parking'];
+                if ($parking === true) {
+                    $parkingCheck = "Si";
+                } else {
+                    $parkingCheck = "No";
+                }
+                ;
+                
+                $vote = $hotel['vote'];
+                // var_dump($vote);
+                $distance = $hotel['distance_to_center'];
+                
+                // var_dump($name, $vote, $description, $parking, $distance);
+                ?>
+               
+                <tr>
+                    <td><?php echo $name; ?></th>
+                    <td><?php echo $description; ?></td>
+                    <td><?php echo $parkingCheck; ?></td>
+                    <td><?php echo $vote; ?>/5</td>
+                    <td><?php echo $distance; ?> Km</td>
+                </tr>
+             <?php   
+            }
+            ?>
 
-
-    ?>
-
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-        <title>Document</title>
-    </head>
-
-    <body>
-
-        <p>
-            <?php echo $name; ?>
-        </p>
-        <p>
-            <?php echo $description; ?>
-        </p>
-        <p>
-            <?php echo $parkingCheck; ?>
-        </p>
-        <p>
-            <?php echo $vote; ?>
-        </p>
-        <p>
-            <?php echo $distance; ?>
-        </p>
-        <br>
-
+            </tbody>
+        </table>
 
     </body>
 
     </html>
 
     <?php
-}
 
 
 
